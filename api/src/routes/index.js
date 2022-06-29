fc2207bcb6ce0197947b9312e9edd346c6058cca
Luis Gimenez <luisconsultor@outlook.com>
2022-06-29 19:38:36 -0300

    commit marvel x recipe

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -9,6 +9,6 @@ const router = Router();
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
-router.use("/marvel", personajes); // api/recipe/*
+router.use("/marvel", personajes); // api/marvel/*
 
 module.exports = router;
